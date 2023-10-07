994d3c9cf1ac0f7b5e037a4340a0ef5702bdb3c8
SumiAkter2 <akteralisha36@gmail.com>
2023-10-07 20:12:58 +0600

    added Header and Footer in Main

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import Home from "../Component/Home/Home";
+import Footer from "../Component/Shared/Footer";
+import Header from "../Component/Shared/Header";
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
