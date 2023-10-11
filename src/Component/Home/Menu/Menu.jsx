66a5f46d04f41b6082cd7d4a70043b8f30a9ba86
SumiAkter2 <akteralisha36@gmail.com>
2023-10-11 21:02:55 +0600

    done

--- src/Component/Home/Menu/Menu.jsx
+++ src/Component/Home/Menu/Menu.jsx
@@ -15,15 +15,15 @@ const Menu = () => {
       <div className="grid grid-cols-2 lg:grid-cols-3 lg:gap-8 gap-4 lg:mx-8 mx-2">
         {menu.map((item) => (
           <div key={item._id}>
-            <div className="card bg-gray-200 hover:bg-gray-100 shadow-xl shadow-gray-400">
+            <div className="card bg-gray-200 lg:hover:bg-gray-100 shadow-xl shadow-gray-400">
               <figure>
                 <img
                   src={item.picture}
                   alt="image"
-                  className="lg:h-64 h-56 w-64 rounded-2xl m-4 lg:w-80"
+                  className="lg:h-72 m-2 h-32 w-32 rounded-2xl lg:w-96"
                 />
               </figure>
-              <div className="card-body ">
+              <div className="p-4 ">
                 <h2 className="card-title">{item.name}</h2>
 
                 <div className="card-actions justify-between items-center">
